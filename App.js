85adf6f31e7eca4e0c47dfe442d0ba0452f79967
donghoonlee94 <94donghoon.lee@gmail.com>
2023-07-24 02:47:19 +0900

    add: image flatlist

--- App.js
+++ App.js
@@ -1,44 +1,40 @@
-import { useState } from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  Image,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useGallery } from "./src/use-gallery";
 
 export default function App() {
-  const [image, setImage] = useState(null);
+  const { images, pickImage } = useGallery();
 
   const onPressOpenGalley = () => {
     pickImage();
   };
 
-    const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
+  const renderItem = ({ item, index }) => {
+    return <Image source={{ uri: item }} style={{ width: 200, height: 200 }} />;
   };
 
-      
   return (
-    <View style={styles.container}>
-      <Button title='갤러리 열기' onPress={onPressOpenGalley} />
-      <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title="갤러리 열기" onPress={onPressOpenGalley} />
+      <FlatList data={images} renderItem={renderItem} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center'
-    
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
 });
--- src/use-gallery.js
+++ src/use-gallery.js
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+
 export const useGallery = () => {
-    
+  const [images, setImage] = useState([]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage([...images, result.assets[0].uri]);
+    }
+  };
+
+  return {
+    pickImage,
+    images,
+  };
 };
